994ba9e15ca1ce619cc418c04d0408ffe5f1387d
Anubhav-rawat <rawatanubhav085@gmail.com>
2024-04-30 23:23:02 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import FilterOptions from "./FilterOptions";
+import MovieList from "./MovieList";
 
-function App() {
+import "./App.css";
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [filterOptions, setFilterOptions] = useState({
+    languages: [],
+    countries: [],
+    genres: [],
+  });
+  const [prevFilteredMovies, setPrevFilteredMovies] = useState([]);
+  const [filteredMovies, setFilteredMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await fetch("/moviesData.json");
+        const data = await response.json();
+        setMovies(data);
+        setFilteredMovies(data);
+      } catch (error) {
+        console.error("Error fetching movies:", error);
+      }
+    };
+
+    fetchMovies();
+  }, []);
+
+  useEffect(() => {
+    let tempMovies = movies;
+    if (filterOptions.languages.length > 0) {
+      tempMovies = tempMovies.filter((movie) =>
+        filterOptions.languages.every((lang) =>
+          movie.movielanguages.includes(lang.target.name)
+        )
+      );
+    }
+    if (filterOptions.countries.length > 0) {
+      tempMovies = tempMovies.filter((movie) =>
+        filterOptions.countries.every((country) =>
+          movie.moviecountries.includes(country.target.name)
+        )
+      );
+    }
+    if (filterOptions.genres.length > 0) {
+      tempMovies = tempMovies.filter((movie) =>
+        filterOptions.genres.every((genre) =>
+          movie.moviegenres.includes(genre.target.name)
+        )
+      );
+    }
+    setPrevFilteredMovies(filteredMovies);
+    setFilteredMovies(tempMovies);
+  }, [filterOptions, movies]);
+
+  const handleFilterChange = (name, value) => {
+    setFilterOptions((prevOptions) => ({
+      ...prevOptions,
+      [name]: value.target.checked
+        ? [...prevOptions[name], value]
+        : prevOptions[name].filter(
+            (option) => option.target.name !== value.target.name
+          ),
+    }));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="app">
+        <FilterOptions
+          options={Array.from(
+            new Set(movies.flatMap((movie) => movie.movielanguages))
+          )}
+          heading="Languages Filter"
+          selectedOptions={filterOptions.languages}
+          onChange={(value) => handleFilterChange("languages", value)}
+        />
+        <FilterOptions
+          options={Array.from(
+            new Set(movies.flatMap((movie) => movie.moviecountries))
+          )}
+          heading="Countries Filter"
+          selectedOptions={filterOptions.countries}
+          onChange={(value) => handleFilterChange("countries", value)}
+        />
+        <FilterOptions
+          options={Array.from(
+            new Set(movies.flatMap((movie) => movie.moviegenres))
+          )}
+          heading="Genres Filter"
+          selectedOptions={filterOptions.genres}
+          onChange={(value) => handleFilterChange("genres", value)}
+        />
+      </div>
+      <MovieList
+        movies={filteredMovies.length > 0 ? filteredMovies : prevFilteredMovies}
+      />
     </div>
   );
-}
+};
 
 export default App;
